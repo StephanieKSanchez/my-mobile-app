157f0011715bedf039dce823660d17d794c13fdb
Stephanie Sanchez <stephaniesanchez@Stephanies-MacBook-Air-2.local>
2022-05-24 15:51:21 -0400

    styled button & added counter

--- App.js
+++ App.js
@@ -1,10 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View , Button} from 'react-native';
+import Counter from './src/components/Counter';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.heading}>Open up App.js to start working on your app!</Text>
+      <Button title="Press me please" />
+      <Counter />
       <StatusBar style="auto" />
     </View>
   );
